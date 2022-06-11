d759577ed2e9e20ebd1caf215c0d7ee00614d87b
Devin Spitz <devin@spitz.onl>
2022-06-11 17:58:48 +0200

    fix some issue and change script path

--- Scripts/Game/NO_DTF/NO_SCR_FactionControlTrigger.c
+++ Scripts/Game/NO_DTF/NO_SCR_FactionControlTrigger.c
@@ -5,19 +5,24 @@ class NO_SCR_FactionControlTriggerClass: SCR_FactionControlTriggerEntityClass
 
 class NO_SCR_FactionControlTrigger: SCR_FactionControlTriggerEntity
 {
+	
+	//Action
+	[Attribute("1", UIWidgets.ComboBox, "What should that trigger do to its parent Task?","", ParamEnumArray.FromEnum(TriggerType), category: "TaskManager:" )]
+	protected TriggerType m_tTriggerType;	
+	
 	IEntity Owner;
-        override void OnInit(IEntity owner)
-        {
-        Owner = owner;
-        super.OnInit(owner);
-        }
+    override void OnInit(IEntity owner)
+    {
+	    Owner = owner;
+	    super.OnInit(owner);
+    }
 
     override void OnActivate(IEntity ent)
     {
         //Finish Task 1
         IEntity taskEntity = GetParent();
         NO_SCR_EditorTask task = NO_SCR_EditorTask.Cast(taskEntity);
-        task.ChangeStateOfTask(TriggerType.Finish);
+        task.ChangeStateOfTask(m_tTriggerType);
 
         
     }
